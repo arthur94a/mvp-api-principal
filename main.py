fafc27d17551e6c70289d0fb3d7cdd796c75a8a8
arthur94a <arthuraraujo94@hotmail.com>
2026-03-20 18:38:55 -0300

    Allow cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from routes.users.route import user_router
 from routes.vehicles.route import vehicle_router
@@ -13,6 +14,19 @@ async def lifespan(app: FastAPI):
     print("Shutting down app")
 
 app = FastAPI(lifespan=lifespan)
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router, prefix="/users", tags=["Users"])
 app.include_router(vehicle_router, prefix="/vehicles", tags=["Vehicles"])
